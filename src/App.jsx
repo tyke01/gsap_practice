7c8d8b8999b77b01eed5735564f0837258bec179
victor Macharia <Techvictor653@gmail.com>
2024-03-17 23:09:59 +0300

    fifth commit: GSAP.Stagger

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import GsapFrom from './pages/GsapFrom'
 import GsapFromTo from './pages/GsapFromTo'
+import GsapStagger from './pages/GsapStagger'
 import GsapTimeline from './pages/GsapTimeline'
 import GsapTo from './pages/GsapTo'
 
@@ -9,11 +10,13 @@ function App() {
 
   return (
     <main className='w-full h-full px-10 py-5'>
-      <GsapTo />
-      <GsapFrom />
-      <GsapFromTo />
+      {/* <GsapTo /> */}
+      {/* <GsapFrom /> */}
+      {/* <GsapFromTo /> */}
 
-      <GsapTimeline />
+      {/* <GsapTimeline /> */}
+
+      <GsapStagger />
     </main>
   )
 }
